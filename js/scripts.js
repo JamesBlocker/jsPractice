90ad7c7140090371c615182bccc94a1c55b9a639
Byron Blocker <byronblocker@gmail.com>
2019-01-25 01:15:42 -0800

    finish leap year calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,16 @@
 // // current date ----------------------------------------------
 // let d = new Date
-
 // function format(n){
 //     return n > 9 ? "" + n: "0" + n;
 // }
-
 // let month = format(d.getMonth() + 1);
 // let day = format(d.getDate());
 // let year = d.getFullYear()
 // let result = month + "/" + day + "/" + year
-
 // document.getElementById("1").innerHTML = result;
 
 
 // // triangle area -------------------------------------------------
-
 // function triArea(a,b,c) {
 //     let p = (a + b + c) / 2;
 //     let pa = p-a
@@ -26,20 +22,39 @@
 //     // console.log(pabc, p, 24 * 9, Math.sqrt(216));
 //     return result;
 // }
-
 // let final = triArea(5,6,7);
 
-// title shuffle
-function shuffleLetters() {
-    let s = document.getElementById("1").innerHTML;
-    let letters = s.split("");
-    let var1 = letters.shift();
-    letters.push(var1);
-    let result = letters.join('');
-    document.getElementById("1").innerHTML = result;
-    return result
+// // title shuffle ------------------------------------------------------------
+// function shuffleLetters() {
+//     let s = document.getElementById("1").innerHTML;
+//     let letters = s.split("");
+//     let var1 = letters.shift();
+//     letters.push(var1);
+//     let result = letters.join('');
+//     document.getElementById("1").innerHTML = result;
+//     return result
+// }
+// document.getElementById("1").innerHTML = "w3source";
+// var timer = setInterval(shuffleLetters, 500);
+
+// leap year -----------------------------------------------------------------
+let d = new Date();
+let year = d.getFullYear();
+function leapYear(y) {
+    if (y % 4 !== 0) {
+        return false;
+    }else if (y % 100 !== 0) {
+        return true;
+    } else if (y % 400 !== 0) {
+        return false;
+    }
+    return true;
 }
 
-document.getElementById("1").innerHTML = "w3source";
+console.log(leapYear(1965));
+if (leapYear(1964)) {
+    document.getElementById("1").innerHTML = "That is a leap year";
+} else {
+    document.getElementById("1").innerHTML = "That is NOT a leap year";
 
-var timer = setInterval(shuffleLetters, 500);
+}
