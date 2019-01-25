c4710222a0029e754cd83121ea1ff34b162400a6
Byron Blocker <byronblocker@gmail.com>
2019-01-25 02:19:08 -0800

    add form and build random number guess game

--- js/scripts.js
+++ js/scripts.js
@@ -37,24 +37,47 @@
 // document.getElementById("1").innerHTML = "w3source";
 // var timer = setInterval(shuffleLetters, 500);
 
-// leap year -----------------------------------------------------------------
-let d = new Date();
-let year = d.getFullYear();
-function leapYear(y) {
-    if (y % 4 !== 0) {
-        return false;
-    }else if (y % 100 !== 0) {
-        return true;
-    } else if (y % 400 !== 0) {
-        return false;
-    }
-    return true;
+// // leap year -----------------------------------------------------------------
+// let d = new Date();
+// let year = d.getFullYear();
+// function leapYear(y) {
+//     if (y % 4 !== 0) {
+//         return false;
+//     }else if (y % 100 !== 0) {
+//         return true;
+//     } else if (y % 400 !== 0) {
+//         return false;
+//     }
+//     return true;
+// }
+
+// console.log(leapYear(1965));
+// if (leapYear(1964)) {
+//     document.getElementById("1").innerHTML = "That is a leap year";
+// } else {
+//     document.getElementById("1").innerHTML = "That is NOT a leap year";
+
+// }
+
+// random integer guess
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max)) + 1;
 }
 
-console.log(leapYear(1965));
-if (leapYear(1964)) {
-    document.getElementById("1").innerHTML = "That is a leap year";
-} else {
-    document.getElementById("1").innerHTML = "That is NOT a leap year";
+document.getElementById("1").innerHTML = "Guess a number between 1-10"
+const form = document.getElementById('form');
+const number = document.getElementById('number');
+form.onsubmit = submit;
 
+function submit(evt) {
+    let input = parseInt(document.getElementById("number").value);
+    let target = parseInt(getRandomInt(10));
+    if (input === target) {
+        document.getElementById("2").innerHTML = ""
+        document.getElementById("1").innerHTML = "Good Work!";
+    } else {
+        document.getElementById("2").innerHTML = "Not matched. The number was " + target;
+    }
+    console.log("!", target, "$", input);
+    event.preventDefault();
 }
