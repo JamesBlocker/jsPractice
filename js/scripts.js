6ac015310df2a8b31cd1371d45741368adc3c97c
Byron Blocker <byronblocker@gmail.com>
2019-01-24 20:51:04 -0800

    add area calculator for triangle from side lengths

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// // current date ----------------------------------------------
+// let d = new Date
+
+// function format(n){
+//     return n > 9 ? "" + n: "0" + n;
+// }
+
+// let month = format(d.getMonth() + 1);
+// let day = format(d.getDate());
+// let year = d.getFullYear()
+// let result = month + "/" + day + "/" + year
+
+// document.getElementById("1").innerHTML = result;
+
+
+// triangle area -------------------------------------------------
+
+function triArea(a,b,c) {
+    let p = (a + b + c) / 2;
+    let pa = p-a
+    let pb = p-b
+    let pc = p-c
+    let pabc = pa * pb * pc;
+    let result = Math.sqrt(p * pabc);  
+    // console.log(result);
+    // console.log(pabc, p, 24 * 9, Math.sqrt(216));
+    return result;
+}
+
+console.log(triArea(5,6,7));
+let area = triArea(5,6,7);
+console.log(area);
+
+
+document.getElementById("1").innerHTML = area;
